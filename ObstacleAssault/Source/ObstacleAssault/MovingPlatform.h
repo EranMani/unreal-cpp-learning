e8a2a867ba5f899e54b6d4c9df62439cbe2205d8
Eran Mani <82909368+EranMani@users.noreply.github.com>
2022-09-30 05:31:19 -0700

    Mark functions as const

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -35,8 +35,8 @@ private:
 
 	void MovePlatform(float DeltaTime);
 	void RotatePlatform(float DeltaTime);
-	bool ShouldPlatformReturn();
-	float GetDistanceMoved();
+	bool ShouldPlatformReturn() const;
+	float GetDistanceMoved() const;
 	
 };
 
